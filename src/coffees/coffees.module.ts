17adfadb3f403a5b294b50c057f2fbb0cf409e4a
Piotr Stachnio <piotrstachnio@gmail.com>
2025-05-22 23:44:39 +0200

    added docker setup, entities

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './cofees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffe.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+  imports: [TypeOrmModule.forFeature([Coffee])],
+})
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,53 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffe.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Arabica',
-      brand: 'Starbucks',
-      flavours: ['chocolate', 'vanilla'],
-    },
-    {
-      id: 2,
-      name: 'Robusta',
-      brand: 'Nescafe',
-      flavours: ['caramel', 'hazelnut'],
-    },
-    {
-      id: 3,
-      name: 'Liberica',
-      brand: 'Blue Bottle',
-      flavours: ['floral', 'citrus'],
-    },
-    {
-      id: 4,
-      name: 'Excelsa',
-      brand: "Peet's Coffee",
-      flavours: ['berry', 'spice'],
-    },
-    {
-      id: 5,
-      name: 'Geisha',
-      brand: 'Intelligentsia',
-      flavours: ['jasmine', 'bergamot'],
-    },
-    {
-      id: 6,
-      name: 'Catimor',
-      brand: 'Lavazza',
-      flavours: ['nutty', 'earthy'],
-    },
-  ];
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
 
   findAll() {
-    return this.coffees;
+    return this.coffeeRepository.find();
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find((item) => item.id === +id);
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
 
     if (!coffee) {
       throw new NotFoundException(`Coffee nr. #${id} not found`);
@@ -57,20 +25,24 @@ export class CoffeesService {
   }
 
   create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
+    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(coffee);
   }
 
-  update(id: string, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id);
-    if (existingCoffee) {
-      // update the existing entity
+  async update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = await this.coffeeRepository.preload({
+      id: +id,
+      ...updateCoffeeDto,
+    });
+
+    if (!existingCoffee) {
+      throw new NotFoundException(`Coffee nr. #${id} not found`);
     }
+    return this.coffeeRepository.save(existingCoffee);
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+  async remove(id: string) {
+    const coffee = await this.findOne(id);
+    return await this.coffeeRepository.remove(coffee);
   }
 }
